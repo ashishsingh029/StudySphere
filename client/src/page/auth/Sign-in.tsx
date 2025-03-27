dce4e3d77de0bc91c72eec30e3b8b9392b58cf9d
Vaibhav Kumar <vaibhav@example.com>
2025-03-27 11:11:41 +0530

    Fix error

--- client/src/page/auth/Sign-in.tsx
+++ client/src/page/auth/Sign-in.tsx
@@ -75,13 +75,16 @@ const SignIn = () => {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
+        <div className="flex flex-row justify-center space-x-2">
+          <Logo />
         <Link
           to="/"
           className="flex items-center gap-2 self-center font-medium"
         >
-          <Logo />
           Team Sync.
         </Link>
+        </div>
+        
         <div className="flex flex-col gap-6">
           <Card>
             <CardHeader className="text-center">
