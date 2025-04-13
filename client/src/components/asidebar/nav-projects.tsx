36d48984e66d502e6575f1c7ca9994e395083a0c
Vaibhav Kumar <vaibhav@example.com>
2025-04-13 23:49:49 +0530

    ui fix

--- client/src/components/asidebar/nav-projects.tsx
+++ client/src/components/asidebar/nav-projects.tsx
@@ -119,7 +119,7 @@ export function NavProjects() {
             </button>
           </PermissionsGuard>
         </SidebarGroupLabel>
-        <SidebarMenu className="h-[18vh] scrollbar overflow-y-auto overflow-x-hidden pb-2">
+        <SidebarMenu className="h-[calc(30vh)] sm:h-[calc(18vh)] scrollbar overflow-y-auto overflow-x-hidden pb-2">
           {isError ? <div>Error occured</div> : null}
           {isPending ? (
             <Loader
